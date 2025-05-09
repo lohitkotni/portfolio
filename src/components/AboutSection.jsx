d1400c2a2744c92e3ebce69f7205dc5ea0adb978
Surya Lohith Kotni <110249804+lohitkotni@users.noreply.github.com>
2025-05-09 16:34:19 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,7 +8,7 @@ export const AboutSection = () => {
           About <span className="text-primary">Me</span>
         </h2>
 
-        <div className="grid grid-cols-2 md:grid-cols-2 gap-12 items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
               Passionate Full Stack Developer
